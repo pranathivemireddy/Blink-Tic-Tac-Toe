db9f61fffa066d3eccf8db62e8394ebf8e0a4381
pranathivemireddy <pranathivemireddy22@gmail.com>
2025-05-23 17:33:31 +0530

    Game initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Game from './Components/Game'
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Blink Tac Toe</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Game.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import '../Game.css'
+
+const Game = () => {
+  return (
+    <div className="game-board">
+      {/* Add your game logic and grid here */}
+      <p>Game Board Coming Soon...</p>
+    </div>
+  );
+};
+
+export default Game;
